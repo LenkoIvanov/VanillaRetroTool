b7ed6f08e5ceb3dba914b0dd952882dc83b8ea6e
blubito-lenko-ivanov-01 <l.ivanov@blubito.com>
2025-01-08 16:19:16 +0200

    refactor: Refactor the wss to utilize a single web socket manager for operations related to connections

--- server/index.js
+++ server/index.js
@@ -1,22 +1,20 @@
 import { WebSocketServer } from 'ws';
 import { wssConfig } from './src/configs/socketServerConfig.js';
-import { connectionService } from './src/services/connectionService.js';
-import { connectionStorageSingleton } from './src/singletons/Connections.js'
+import { wssManager } from './src/singletons/WebSocketManager.js'
 
 const wss = new WebSocketServer(wssConfig);
 
-wss.on('connection', function connection(ws, request) {
-    ws.send('Connection established');
-    connectionService.addNewConnection('blaaa')
+wss.on('connection', function connection(ws) {
+    wssManager.addConnection(ws);
+    wssManager.pingConnection(ws, 'Connected to web socket');
   
     ws.on('message', function message(data) {
-        const parsedData = JSON.parse(data);
-        console.log('Test connection: ', JSON.stringify(connectionStorageSingleton.getConnections))
-        console.log('Message recieved: ', parsedData.message);
+        wssManager.receiveDataFromConnection(data, (parsed) => {
+          console.log("Do something with connection data: ", parsed)
+        })
     });
 
     ws.on('close', function () {
-      connectionService.removeNewConnection('blaaa');
-      console.log('Test connection: ', JSON.stringify(connectionStorageSingleton.getConnections))
+      wssManager.removeConnection(ws)
     });
 });
