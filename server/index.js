278c8e60ae96728588c3d6408bb499c8610d93a7
blubito-lenko-ivanov-01 <l.ivanov@blubito.com>
2025-01-14 16:46:59 +0200

    feat: Add retro note service and tweak singletons

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import { WebSocketServer } from 'ws';
 import { wssConfig } from './src/configs/socketServerConfig.js';
 import { wssManager } from './src/singletons/WebSocketManager.js'
+import { retroNotesService } from './src/services/retroNotesService.js';
 
 const wss = new WebSocketServer(wssConfig);
 
@@ -9,9 +10,18 @@ wss.on('connection', function connection(ws) {
     wssManager.pingConnection(ws, 'Connected to web socket');
   
     ws.on('message', function message(data) {
+      try{
         wssManager.receiveDataFromConnection(data, (parsed) => {
-          console.log("Do something with connection data: ", parsed)
+          console.log("Received new note: ", parsed);
+          retroNotesService.addNewNote(parsed);
         })
+      }catch(e){
+        console.log("An error encountered while saving a new note: ", e);
+      } finally {
+        const payload = JSON.stringify(retroNotesService.getAllNotes());
+        console.log("Broadcast payload: ", payload);
+        wssManager.broadcast(payload);
+      }
     });
 
     ws.on('close', function () {
---
+++ server/src/services/retroNotesService.js
@@ -0,0 +1,23 @@
+import { RetroNote, notesStorageSingleton } from "../singletons/RetroNotes.js";
+
+class RetroNotesService {
+    constructor(){
+    }
+
+    addNewNote(request) {
+        console.log('Request', request);
+        if(request.creatorId && request.topic && request.text) {
+            const retroNote = new RetroNote(request.creatorId, request.text, request.topic);
+            notesStorageSingleton.addNewNote(retroNote);
+        }
+    }
+
+    getAllNotes(){
+        return {
+            notes: notesStorageSingleton.getNotes()
+        }
+    }
+
+}
+
+export const retroNotesService = new RetroNotesService();
