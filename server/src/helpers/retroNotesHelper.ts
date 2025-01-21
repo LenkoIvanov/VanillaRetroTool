bbe6010039228640b3a5300503c6c0b9fe688a5a
blubito-lenko-ivanov-01 <l.ivanov@blubito.com>
2025-01-21 17:30:25 +0200

    refactor: validate the client data with Zod

--- server/src/helpers/retroNotesHelper.ts
+++ server/src/helpers/retroNotesHelper.ts
@@ -1,9 +1,11 @@
+import { retroNoteSchema } from '../schemas/retroNoteSchema.js';
 import type { RetroNoteData } from '../types/RetroNoteData.js';
 
 export const parseRetroNote = (data: string): RetroNoteData => {
   try {
-    const parsedData: RetroNoteData = JSON.parse(data);
-    return parsedData;
+    const parsedData = JSON.parse(data);
+    const validatedRetroNote: RetroNoteData = retroNoteSchema.parse(parsedData);
+    return validatedRetroNote;
   } catch (err) {
     throw new Error('An error was encountered while parsing to a retro note');
   }
